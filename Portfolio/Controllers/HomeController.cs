40a527820db20a099228cb0c755da64f1c8ee9b6
werdi51 <spotifig2@gmail.com>
2026-06-19 17:15:20 +0500

    site

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Achievements()
+        {
+            return View();
+        }
+
+        public IActionResult Works()
+        {
+            return View();
+        }
+
+        public IActionResult Documents()
+        {
+            return View();
+        }
+
+        public IActionResult Citizenship()
+        {
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
