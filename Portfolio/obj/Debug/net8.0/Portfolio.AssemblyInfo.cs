e5cd135acf8ef394eb2556ca64c0ddf54cd9602e
werdi51 <spotifig2@gmail.com>
2026-06-28 17:57:52 +0500

    final

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a527820db20a099228cb0c755da64f1c8ee9b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d0664bf1a414b03220f61421c2f105eda105da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
